73b124f101655bdd0742736c18b687c86ad0aa03
ArnoldOlanda <olanda188@gmail.com>
2021-11-30 11:35:28 -0500

    Codigo del ejercicio agregado

---
+++ ejercicio/server.php
@@ -0,0 +1,7 @@
+<?PHP
+if ($_REQUEST['data'] == "venta")
+    echo "<tr><th>Fecha</th><th>Cliente</th> <th>Total Venta</th></tr>
+    <tr><td>12/05/2021</td><td>Jose</td><td>S/. 350 </td></tr>
+    <tr><td>02/05/2021</td><td>Carlos</td><td>S/. 250 </td></tr>
+    <tr><td>20/05/2021</td><td>Arnold</td><td>S/. 100 </td></tr>
+    <tr><td>10/05/2021</td><td>Maria</td><td>S/. 230 </td></tr>";
